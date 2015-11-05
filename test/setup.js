97757a676808e5610a1647adb67d7b4a83f1d535
Max <maxim-but@mail.ru>
2015-11-05 19:44:22 +0300

    Base files for project

---
+++ test/setup.js
@@ -0,0 +1,5 @@
+global._	   = require("underscore");
+global.should  = require("should");
+global.expect  = require("expect.js");
+global.chai    = require("chai");
+global.mockify = require("muon-mockify");
